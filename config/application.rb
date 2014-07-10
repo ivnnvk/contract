dab164a748c27cb62b228f6caf333f1d8bdc547e
Ivan Novak <ivnnvk@gmail.com>
2014-07-10 09:08:21 +0200

    views defauls improve

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Contract01
   class Application < Rails::Application
     config.i18n.default_locale = :cs
+    config.time_zone = 'Prague'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
