92daac2dec486d0d20f88b810b514eaf026bf562
Ivan Novak <ivnnvk@gmail.com>
2014-08-09 17:34:08 +0200

    fining export of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Employee.create("user_name"=>"pzachar", "name"=>"Pavel Zachar", "hour_rate"=>500)
-Employee.create("user_name"=>"jplacha", "name"=>"Jana Plachá", "hour_rate"=>500)
-Source.create("description"=>"Ford\r\n1S1 7568", "unit"=>"km", "unit_price"=>4.2, 
-	"source_type_id"=>1)
-Source.create("description"=>"Fabia\r\n1ST 2043", "unit"=>"km", "unit_price"=>4.2, 
-	"source_type_id"=>1)
-Source.create("description"=>"", "unit"=>"ks", "unit_price"=>150, "source_type_id"=>2)
-Source.create("description"=>"nastřelovací hřebíky", "unit"=>"kg", "unit_price"=>160, 
-	"source_type_id"=>3)
+Employee.create("user_name"=>"pzachar", "name"=>"Pavel Zachar", "hour_rate"=>500.0)
+Employee.create("user_name"=>"jplacha", "name"=>"Jana Plachá", "hour_rate"=>500.0)
+Source.create("description"=>"Ford\r\n1S1 7568", "unit"=>"km", "source_type_id"=>1, "unit_price"=>4.2)
+Source.create("description"=>"Fabia\r\n1ST 2043", "unit"=>"km", "source_type_id"=>1, "unit_price"=>4.2)
+Source.create("description"=>"", "unit"=>"ks", "source_type_id"=>2, "unit_price"=>150.0)
+Source.create("description"=>"nastřelovací hřebíky", "unit"=>"kg", "source_type_id"=>3, "unit_price"=>160.0)
 SourceType.create("name"=>"auto")
 SourceType.create("name"=>"mezník")
 SourceType.create("name"=>"ostatní")
-Customer.create("name"=>"Hooktes", "place"=>"Krákorova 123\r\nMladá Vožice", 
-	"representant"=>"Pavel Hujer", "phone"=>"725232737")
+Customer.create("name"=>"Hooktes", "place"=>"Krákorova 123\r\nMladá Vožice", "representant"=>"Pavel Hujer", "phone"=>"725232737")
 Township.create("name"=>"Kolín")
 CadastralOffice.create("name"=>"Kolín")
 OrderWay.create("name"=>"telefonicky")
@@ -36,25 +32,7 @@ PriceCreation.create("name"=>"výpočet")
 PriceCreation.create("name"=>"dle obvyklého ceníku")
 PriceCreation.create("name"=>"nedomluvena")
 PriceCreation.create("name"=>"bude upřesněna - kdy a jak")
-Contract.create("contract_number"=>"00209087-91/2014", "record_number"=>"00177563", 
-	"cadastral_territory"=>"122", "regarding_plat_numbers"=>"256/2, 266/8", 
-	"acceptance_date"=>"Sun, 01 Jun 2014", "fulfilment_term"=>"Tue, 15 Jul 2014", 
-	"contract_description"=>"Zaměřit barák\r\na taky pozemek", 
-	"order_number"=>121, "ordered_by"=>"Plachá", "order_phone"=>"123456798", 
-	"bend_date"=>"Tue, 10 Jun 2014", "make_out_date"=>"Fri, 20 Jun 2014", 
-	"check_date"=>"Sun, 22 Jun 2014", "unfulfill_reason"=>"", 
-	"sketch_number"=>"21254", "screened_date"=>"Wed, 25 Jun 2014", 
-	"handover_date"=>"Thu, 26 Jun 2014", 
-	"deliver_address"=>"Kolín\r\nKolínova 265", "agreed_price"=>15255, 
-	"price_creation_specification"=>"", "information_from_CO_ect"=>"na katastru to zaevidovali", 
-	"invoicing_foundations"=>"fakturovat co nejdříve", "other_important_particulars"=>"", 
-	"customer_id"=>1, "cadastral_office_id"=>1, "township_id"=>1, "responsible_id"=>1, 
-	"bend_id"=>2, "drawed_up_id"=>1, "checked_id"=>1, "screened_id"=>1, "order_way_id"=>2, 
-	"order_type_id"=>2, "handover_way_id"=>2, "price_creation_id"=>1)
-Invoice.create("number"=>"1365/14", "amount"=>15255, "issue_date"=>"Mon, 30 Jun 2014", 
-	"due_date"=>"Tue, 15 Jul 2014", "contract_id"=>1)
-WorkDiary.create("doing"=>"vyměřování a zaměřování","from"=>"Mon, 30 Jun 2014 08:00:00 UTC +00:00", 
-	"to"=>"Mon, 30 Jun 2014 12:00:00 UTC +00:00", "employee_id"=>1, "contract_id"=>1)
-UsageSource.create("from"=>"Mon, 30 Jun 2014 08:00:00 UTC +00:00", 
-	"to"=>"Mon, 30 Jun 2014 12:00:00 UTC +00:00", "unit_number"=>70, "amount"=>nil, 
-	"source_id"=>2, "contract_id"=>1, "employee_id"=>1)
+Contract.create("contract_number"=>"00209087-91/2014", "record_number"=>"00177563", "cadastral_territory"=>"122", "regarding_plat_numbers"=>"256/2, 266/8", "contract_description"=>"Zaměřit barák\r\na taky pozemek", "order_number"=>"121", "ordered_by"=>"Plachá", "order_phone"=>"123456798", "unfulfill_reason"=>"", "sketch_number"=>"21254", "deliver_address"=>"Kolín\r\nKolínova 265", "price_creation_specification"=>"", "information_from_CO_ect"=>"na katastru to zaevidovali", "invoicing_foundations"=>"fakturovat co nejdříve", "other_important_particulars"=>"", "customer_id"=>1, "cadastral_office_id"=>1, "township_id"=>1, "responsible_id"=>1, "bend_id"=>2, "drawed_up_id"=>1, "checked_id"=>1, "screened_id"=>1, "order_way_id"=>2, "order_type_id"=>2, "handover_way_id"=>2, "price_creation_id"=>1, "agreed_price"=>15255.0, "acceptance_date"=>"2014-06-01", "fulfilment_term"=>"2014-07-15", "bend_date"=>"2014-06-10", "make_out_date"=>"2014-06-20", "check_date"=>"2014-06-22", "screened_date"=>"2014-06-25", "handover_date"=>"2014-06-26")
+Invoice.create("number"=>"1365/14", "contract_id"=>1, "amount"=>15255.0, "issue_date"=>"2014-06-30", "due_date"=>"2014-07-15")
+WorkDiary.create("doing"=>"vyměřování a zaměřování", "employee_id"=>1, "contract_id"=>1, "from"=>"2014-06-30 10:00:00 +0200", "to"=>"2014-06-30 14:00:00 +0200")
+UsageSource.create("amount"=>nil, "source_id"=>2, "contract_id"=>1, "employee_id"=>1, "unit_number"=>70.0, "from"=>"2014-06-30 10:00:00 +0200", "to"=>"2014-06-30 14:00:00 +0200")
--- lib/tasks/export.rake
+++ lib/tasks/export.rake
@@ -2,10 +2,12 @@ namespace :export do
   desc "Prints Employee, Source, SourceType, Customer in a seeds.rb way."
   task :seeds_format => :environment do
     Employee.order(:id).all.each do |r|
-      puts "Employee.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "Employee.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','hour_rate'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"hour_rate\"=>", r.hour_rate, ")\n"
     end
     Source.order(:id).all.each do |r|
-      puts "Source.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "Source.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','unit_price'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"unit_price\"=>", r.unit_price, ")\n"
     end
     SourceType.order(:id).all.each do |r|
       puts "SourceType.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
@@ -32,16 +34,22 @@ namespace :export do
       puts "PriceCreation.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
     end
     Contract.order(:id).all.each do |r|
-      puts "Contract.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "Contract.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','agreed_price','acceptance_date','fulfilment_term','bend_date','make_out_date','check_date','screened_date','handover_date'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"agreed_price\"=>", r.agreed_price, ", \"acceptance_date\"=>\"", r.acceptance_date, "\", \"fulfilment_term\"=>\"", r.fulfilment_term, 
+       "\", \"bend_date\"=>\"", r.bend_date, "\", \"make_out_date\"=>\"", r.make_out_date, "\", \"check_date\"=>\"", r.check_date,
+       "\", \"screened_date\"=>\"", r.screened_date, "\", \"handover_date\"=>\"", r.handover_date, "\")\n"
     end
     Invoice.order(:id).all.each do |r|
-      puts "Invoice.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "Invoice.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','amount','issue_date','due_date'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"amount\"=>", r.amount, ", \"issue_date\"=>\"", r.issue_date, "\", \"due_date\"=>\"", r.due_date, "\")\n"
     end
     WorkDiary.order(:id).all.each do |r|
-      puts "WorkDiary.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "WorkDiary.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','from','to'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"from\"=>\"", r.from, "\", \"to\"=>\"", r.to, "\")\n"
     end
     UsageSource.order(:id).all.each do |r|
-      puts "UsageSource.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id'].include?(key)}.to_s.gsub(/[{}]/,'')})"
+      print "UsageSource.create(#{r.serializable_hash.delete_if {|key, value| ['created_at','updated_at','id','unit_number','from','to'].include?(key)}.to_s.gsub(/[{}]/,'')}",
+       ", \"unit_number\"=>", r.unit_number, ", \"from\"=>\"", r.from, "\", \"to\"=>\"", r.to, "\")\n"
     end
   end
 end
